81d4eaa9816f920b373d1cac38c1099e8f8f4f75
AndrewToledo <toledoandrew3@gmail.com>
2025-02-10 14:55:03 -0300

    Resolvendo bugs

--- src/app/pensamentos/criar-pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/pensamentos/criar-pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamentos/pensamentos';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -6,24 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamentos.component.css'],
 })
 export class ListarPensamentosComponent implements OnInit {
-  listasPensamento = [
-    {
-      conteudo: 'Passo informações para o component filho',
-      autoria: 'Component pai',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Component filho',
-      modelo: 'modelo2',
-    },
-    {
-      conteudo:
-        'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
-      autoria: '',
-      modelo: 'modelo1',
-    },
-  ];
+  listasPensamento: Pensamento[] = [];
 
   constructor() {}
 
